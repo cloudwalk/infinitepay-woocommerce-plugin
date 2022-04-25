60576afc20aa73b7fbfb5b13e9503fb77c02eecb
João Escudero <joaovescudero@gmail.com>
2022-04-25 09:32:16 -0300

    feat: first commit :rocket:

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4dc1322174816c3a0759c666c9ad1565');
---
+++ templates/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Part of Woo InfinitePay Module
+ * Author - InfinitePay
+ * Developer
+ * Copyright - Copyright(c) InfinitePay [https://www.infinitepay.io]
+ * License - https://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ *
+ * @package InfinitePay
+ */
+exit;
