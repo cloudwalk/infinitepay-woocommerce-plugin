36a3d8183ab83670ac5c4ef6bbcbde435dc7e285
Gabriel Mazurco <gabriel.mazurco@cloudwalk.io>
2022-06-10 13:32:08 -0300

    feat: added pix valid request

--- woocommerce-infinitepay/includes/class-wc-rest-custom-controller.php
+++ woocommerce-infinitepay/includes/class-wc-rest-custom-controller.php
@@ -18,23 +18,24 @@ class WC_REST_Custom_Controller {
 	protected $pix_callback_endpoint = 'infinitepay_pix_callback';
 
 	// Callback handler
-	public function infinite_pay_callback($data) {
+	public function infinite_pay_callback(WP_REST_Request $data) {
 		global $woocommerce;
 
 		// Retrieve parameters
 		$orderId = $data['order_id'];
-		$safetyHash = $data('X-Callback-Signature');
+		$safetyHash = $data->get_header('X-Callback-Signature');
 
 		// Retrieve order
 		$order = wc_get_order($orderId);
 		$transactionIds = get_post_meta($orderId, 'transactionSecret');
-		$convertedTransactionId = hash_hmac('sha256', $data, $transactionIds[0], false);
+		$body = json_encode($data->get_json_params());
+		$convertedTransactionId = hash_hmac('sha256', $body, $transactionIds[0], false);
 
 		// Validate if the request is valid
 		if ($convertedTransactionId != $safetyHash) {
 			// Return bad request
 			return array(
-				'status' => 200
+				'status' => 400
 			);
 		}
 
@@ -42,14 +43,17 @@ class WC_REST_Custom_Controller {
 		$paymentReceivedStatus = 'processing';
 		$order->update_status($paymentReceivedStatus);
 
+		$nsu = get_post_meta($orderId, 'nsuHost');
+
 		// Returning
 		return array(
 			'status' => 200,
+			'nsu' => $nsu,
 			'orderId' => $orderId,
+			'safety' => $safetyHash,
+			'body' => $body,
 			'transactionId' => $transactionIds[0],
-			'convertedTransactionId' => $convertedTransactionId,
-			'postId' => $post_id,
-			'order' => $order
+			'convertedTransactionId' => $convertedTransactionId
 		);
 	}
 
@@ -60,7 +64,7 @@ class WC_REST_Custom_Controller {
 			'/' . $this->pix_callback_endpoint,
 			array(
 				'methods' => 'POST',
-				'callback' => array( $this, 'infinite_pay_callback' )
+				'callback' => array($this, 'infinite_pay_callback')
 			)
 		);
 	}
