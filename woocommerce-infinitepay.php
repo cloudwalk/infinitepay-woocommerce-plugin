3d8867a73e20a9897116cff9f5ea2327d473a049
João Escudero <joaovescudero@gmail.com>
2022-06-24 17:57:44 -0300

    fix: wrong number when using plugins that change order amount

--- woocommerce-infinitepay.php
+++ woocommerce-infinitepay.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: InfinitePay payments for WooCommerce
  * Description: Configure the payment options and accept payments with cards.
- * Version: 1.1.6
+ * Version: 1.1.7
  * Author: Infinite Pay
  * Author URI: https://infinitepay.io/
  * Text Domain: infinitepay-woocommerce
