ab7737b71ae7703a42a6da44ac3853edd54f514f
bru-carenzi <bruna.carenzi.github@cloudwalk.io>
2023-06-16 17:05:38 -0300

    fix: add secrets variables

--- e2e_tests_api/features/support/create_credential_base_url.rb
+++ e2e_tests_api/features/support/create_credential_base_url.rb
@@ -5,7 +5,7 @@ module Credentials
       def create_credential_user
         base_uri 'https://infinitepay-api-v2-dot-infinitepay-staging.rj.r.appspot.com/users/'
         format :json
-        headers 'Content-type': 'application/json', 'Authorization': "9ea3be77c34fbd2f0e5d5be83853e866" #ENV['API_KEY']
+        headers 'Content-type': 'application/json', 'Authorization': ENV['API_KEY']
       end
 
     end
@@ -16,7 +16,7 @@ module ErrorCredential
     def create_credential_invalid_user
         base_uri 'https://infinitepay-api-v2-dot-infinitepay-staging.rj.r.appspot.com/users/'
         format :json
-        headers 'Content-type': 'application/json', 'Authorization': "881993493fe1141fd7b3045d4c7396bb" #ENV['API_KEY_INVALID']
+        headers 'Content-type': 'application/json', 'Authorization': ENV['API_KEY_INVALID']
     end
 
 end
