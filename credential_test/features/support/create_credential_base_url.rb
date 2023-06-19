559904c6d7399f4f6d71d118cd77f6db421644f6
bru-carenzi <bruna.carenzi.github@cloudwalk.io>
2023-06-19 09:43:47 -0300

    update API key variable and create yml file

--- credential_test/features/support/create_credential_base_url.rb
+++ credential_test/features/support/create_credential_base_url.rb
@@ -5,9 +5,9 @@ module Credentials
       def create_credential_user
         base_uri 'https://infinitepay-api-v2-dot-infinitepay-staging.rj.r.appspot.com/users/'
         format :json
-        headers 'Content-type': 'application/json', 'Authorization': '9ea3be77c34fbd2f0e5d5be83853e866', 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, br', 'User-Agent': 'PostmanRuntime/7.29.0'
+        headers 'Content-type': 'application/json', 'Authorization': ENV['API_KEY'], 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, br', 'User-Agent': 'PostmanRuntime/7.29.0'
       end
-#ENV['API_KEY']
+
     end
 end
 
@@ -16,8 +16,7 @@ module ErrorCredential
     def create_credential_invalid_user
         base_uri 'https://infinitepay-api-v2-dot-infinitepay-staging.rj.r.appspot.com/users/'
         format :json
-        headers 'Content-type': 'application/json', 'Authorization': '881993493fe1141fd7b3045d4c7396bb', 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, br', 'User-Agent': 'PostmanRuntime/7.29.0'
-        #ENV['API_KEY_INVALID']
+        headers 'Content-type': 'application/json', 'Authorization': ENV['API_KEY_INVALID'], 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, br', 'User-Agent': 'PostmanRuntime/7.29.0'
     end
 
 end
